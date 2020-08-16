c35527b267ea374deefaa69596e85937dc207fd8
brian <riceb53@gmail.com>
2020-08-16 12:26:23 -0400

    pass props from parent to child

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="note-container">
+    <note-selectors />
+    <note-editor />
+  </div>
+</template>
+
+
+<script>
+import NoteSelectors from "./NoteSelectors.vue"
+import NoteEditor from "./NoteEditor.vue"
+
+export default {
+  name: "note-container",
+  components: {
+    NoteSelectors,
+    NoteEditor
+  }
+}
+</script>
