f10c21cdc10b13321a409b5b8dba071f5a84ea1f
brian <riceb53@gmail.com>
2020-08-16 12:58:48 -0400

    change active status using props and

--- src/components/NoteSelector.vue
+++ src/components/NoteSelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="note-selector active">
+  <div class="note-selector" v-bind:class="{active: note.id === selectedNoteId}" v-on:click="changeSelectedNote(note)">
     <p class="note-selector-title">{{note.body}}</p>
     <p class="note-selector-timestamp">{{note.timestamp}}</p>
   </div>
@@ -8,6 +8,11 @@
 <script>
 export default {
   name: 'note-selector',
-  props: ["note"]
+  props: ["note", "selectedNoteId"],
+  methods: {
+    changeSelectedNote: function(note) {      
+      this.$emit("changeSelectedNote", note)
+    }
+  }
 }
 </script>
